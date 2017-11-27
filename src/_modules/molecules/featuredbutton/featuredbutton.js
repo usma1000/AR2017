c67f7fbfc302b8fb17dfcdc298b7b7750c449cfb
usma1000 <usma1000@gmail.com>
2017-11-27 11:48:51 -0500

    created featured button molecule

---
+++ src/_modules/molecules/featuredbutton/featuredbutton.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Constructor
+var Featuredbutton = function() {
+  this.name = 'featuredbutton';
+  console.log('%s module', this.name.toLowerCase());
+};
+
+module.exports = Featuredbutton;
---
+++ src/_modules/molecules/featuredbutton/tests/featuredbutton.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var Featuredbutton = require('../featuredbutton');
+
+describe('Featuredbutton View', function() {
+
+  beforeEach(function() {
+    this.featuredbutton = new Featuredbutton();
+  });
+
+  it('Should run a few assertions', function() {
+    expect(this.featuredbutton);
+  });
+
+});
